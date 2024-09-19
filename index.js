555e35175a31902ae8127178ed591f87bf73c8f4
MadhusreeJ <sreemaddy18@gmail.com>
2024-09-19 23:10:14 +0530

    updated

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ app.listen(port,()=>{
 console.log("Webser running");
 })
  
-const d = new Date();
-let date = d.toJSON().slice(0,10);
-let time = d.getHours() + "-" + d.getMinutes()
 
 app.get("/createfile",(req,res) =>{
-    fs.writeFile(`D:/Daily task/Node/Node_Day1/saved_files/${date}_${time}.txt`,`${d}`, function (err) {
+    const d = new Date();
+    let date = d.toJSON().slice(0,10);
+    let time = d.getHours() + "-" + d.getMinutes()
+    const file = path.join('saved_files', `${date}_${time}.txt`);
+    let data = d.toString();
+
+    fs.writeFile(file, data, function (err) {
         if (err) {
          res.json({message:"Something went wrong"});
          return console.log(err);
@@ -25,8 +28,8 @@ app.get("/createfile",(req,res) =>{
 })
 
 
-const folder = 'D:/Daily task/Node/Node_Day1/saved_files/';
 app.get("/retrievefiles",(req,res)=>{
+    const folder ='saved_files'
     fs.readdir(folder, (err, files) => {
         if (err) {
           console.log(err);
@@ -34,6 +37,7 @@ app.get("/retrievefiles",(req,res)=>{
         } else {
           const allTextFiles = files.filter((file) => path.extname(file) === '.txt');
           res.json(allTextFiles);
+          console.log("Successfully retrieved text files")
         }
       });
 })
